9d9dc360f8f059341ed986de4dec27832d83e439
Vinzzztty <kevinarnandes21@gmail.com>
2023-05-01 23:21:15 +0700

    Make routes request

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
+require("dotenv").config();
+
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
+const connectDB = require("./server/config/dbConfig");
 
 const app = express();
 const port = 5000 || process.env.PORT;
 
+// Connect to Database
+connectDB();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Static Files
+app.use(express.static("public"));
+
 // Templating Engine
 app.use(expressLayout);
+app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Halo",
-    });
-});
+// app.get("/", (req, res) => {
+//     res.render("index");
+// });
+
+// Routes
+app.use("/", require("./server/routes/userRoute"));
 
 app.get("*", (req, res) => {
     res.status(404).json({
